ec6b182c5e1bfd53c1f4708d624c9f3fe16d4c47
Roniel <roniel_ramos@hotmail.com>
2020-11-28 01:32:18 -0300

    test(useCase): montar cenários para testes futuros
    - Validar chamada interna
    - Validar retorno

---
+++ src/travel-route/use-cases/CreateTravelRoute.unit.spec.ts
@@ -0,0 +1,6 @@
+describe('UNIT | CreateTravelRoute', () => {
+  context('When I have to create a new route', () => {
+    it('Should call the persistence method')
+    it('Should not throw an exception')
+  })
+})
