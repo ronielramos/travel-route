529e95ea9fbfe11193b4daa0223370f7e087f6ee
Roniel <roniel_ramos@hotmail.com>
2020-11-28 01:30:05 -0300

    test(Domain): Criar casos de teste para busca gulosa e em profundidade

--- src/travel-route/domain/BestTravelRoute.unit.spec.ts
+++ src/travel-route/domain/BestTravelRoute.unit.spec.ts
@@ -1,40 +1,85 @@
-import { expect } from 'chai'
+import { should } from 'chai'
 
-import FakeTravelRouteRepository from '../infra/database/file/FakeTravelRoute.repository'
-import { ITravelRouteRepository } from '../infra/database/ITravelRoute.repository'
+import GetTravelRouteDTO from '../dtos/GetTravelRoute.dto'
 import BestTravelRoute from './BestTravelRoute'
 
-/**
- * NODES:
- * GRU
- * BRC
- * SCL
- * ORL
- * CDG
-*/
+describe('UNIT | BestTravelRoute', () => {
+  let travelRoutes: GetTravelRouteDTO[]
 
-describe('UNIT | RoutePrice', () => {
-  let travelRouteRepository: ITravelRouteRepository
-
-  beforeEach(() => {
-    travelRouteRepository = new FakeTravelRouteRepository()
+  before(() => {
+    should()
   })
 
-  context('When i have to calculate the best price for a route', () => {
-    it('Should search the lowest price route for GRU-CGD', () => {
-      const routeLowestPrice = new BestTravelRoute(travelRouteRepository)
+  beforeEach(async () => {
+    travelRoutes = [
+      { origin: 'GRU', destination: 'BRC', price: 10 },
+      { origin: 'BRC', destination: 'SCL', price: 5 },
+      { origin: 'GRU', destination: 'CDG', price: 75 },
+      { origin: 'GRU', destination: 'SCL', price: 20 },
+      { origin: 'GRU', destination: 'ORL', price: 56 },
+      { origin: 'ORL', destination: 'CDG', price: 5 },
+      { origin: 'SCL', destination: 'ORL', price: 20 },
 
-      const route = routeLowestPrice.find('GRU', 'CGD')
+      { origin: 'BRC', destination: 'GRU', price: 20 },
+      { origin: 'BRC', destination: 'CDG', price: 20 }
+    ]
+  })
 
-      expect(route).to.be.equal('GRU - BRC - SCL - ORL - CDG')
+  context('When i have to calculate the best price for a travel route', () => {
+    it('Should search route for GRU-CGD', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'GRU', destination: 'CDG' }, travelRoutes)
+
+      route.should.be.equal('GRU - BRC - SCL - ORL - CDG')
     })
 
-    it('Should search the lowest price route for GRU-ORL', () => {
-      const routeLowestPrice = new BestTravelRoute(travelRouteRepository)
+    it('Should search route for GRU-ORL', () => {
+      const bestTravelRoute = new BestTravelRoute()
 
-      const route = routeLowestPrice.find('GRU', 'ORL')
+      const route = bestTravelRoute.find({ origin: 'GRU', destination: 'ORL' }, travelRoutes)
 
-      expect(route).to.be.equal('GRU - BRC - SCL - ORL')
+      route.should.be.equal('GRU - BRC - SCL - ORL')
+    })
+
+    it('Should search route for BRC-ORL', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'BRC', destination: 'ORL' }, travelRoutes)
+
+      route.should.be.equal('BRC - SCL - ORL')
+    })
+
+    it('Should search route for CDG-GRU', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'CDG', destination: 'GRU' }, travelRoutes)
+
+      route.should.be.equal('')
+    })
+
+    it('Should search route for ORL-BRC', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'ORL', destination: 'BRC' }, travelRoutes)
+
+      route.should.be.equal('')
+    })
+
+    it('Should search route for BRC-CDG', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'BRC', destination: 'CDG' }, travelRoutes)
+
+      route.should.be.equal('BRC - CDG')
+    })
+
+    it('Should search route for BRC-GRU', () => {
+      const bestTravelRoute = new BestTravelRoute()
+
+      const route = bestTravelRoute.find({ origin: 'BRC', destination: 'GRU' }, travelRoutes)
+
+      route.should.be.equal('BRC - GRU')
     })
   })
 })
