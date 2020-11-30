eca37d291af086a56a323835b50332aaf871fa6a
Roniel <roniel_ramos@hotmail.com>
2020-11-29 21:56:11 -0300

    feat: implementar busca da melhor rota e criação de uma nova rota na controller do express

--- src/modules/travel-route/infra/http/express/TravelRoute.controller.ts
+++ src/modules/travel-route/infra/http/express/TravelRoute.controller.ts
@@ -1,3 +1,42 @@
-export default class TravelRouteController {
+import { NextFunction, Request, Response } from 'express'
 
+import { CreatedTravelRouteDTO, TravelRouteToCreateDTO } from '../../../dtos/CreateTravelRoute.dto'
+import { TravelRouteFoundDTO, TravelRouteToFindDTO } from '../../../dtos/GetTravelRoute.dto'
+import { IUseCase } from '../../../use-cases/IUseCase'
+
+export default class TravelRouteController {
+  constructor (
+    private createTravelRoute: IUseCase<TravelRouteToCreateDTO, CreatedTravelRouteDTO>,
+    private getBestTravelRoute: IUseCase<TravelRouteToFindDTO, TravelRouteFoundDTO>
+  ) {}
+
+  async create (
+    req: Request<never, never, TravelRouteToCreateDTO, never>,
+    res: Response<CreatedTravelRouteDTO>,
+    next: NextFunction
+  ) {
+    try {
+      const travelRouteToCreate = req.body
+      const createdTravelRoute = await this.createTravelRoute.execute(travelRouteToCreate)
+
+      res.status(201).json(createdTravelRoute)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getOne (
+    req: Request<never, never, TravelRouteToFindDTO, never>,
+    res: Response<{ bestTravelFound: string }>,
+    next: NextFunction
+  ) {
+    try {
+      const travelRouteToFind = req.body
+      const bestTravelFound = await this.getBestTravelRoute.execute(travelRouteToFind)
+
+      res.status(200).json({ bestTravelFound })
+    } catch (error) {
+      next(error)
+    }
+  }
 }
