2ad57888e2d29f8ed19ab625358daf9f3a3f5551
Roniel <roniel_ramos@hotmail.com>
2020-11-30 23:34:49 -0300

    test(CreateTravelRoute): trocar forma de inicialização
    - criar arquivos necessários para inicialização do repositório
    - deletar arquivos criados depois de concluir testes

--- src/modules/travel-route/use-cases/create-travel-route/CreateTravelRoute.integration.spec.ts
+++ src/modules/travel-route/use-cases/create-travel-route/CreateTravelRoute.integration.spec.ts
@@ -1,20 +1,38 @@
-import CreateTravelRoute from './CreateTravelRoute'
 import chai, { expect, should } from 'chai'
-import TravelRouteRepository from '../../infra/database/file/TravelRoute.repository'
 import chaiAsPromised from 'chai-as-promised'
-import { promisify } from 'util'
 import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+
+import { fileAccess } from '../../../../shared/providers/file-access'
+import TravelRouteRepository from '../../infra/database/implementations/TravelRoute.repository'
+import { ITravelRouteRepository } from '../../infra/database/ITravelRoute.repository'
+import CreateTravelRoute from './CreateTravelRoute'
 
 describe('INTEGRATION | CreateTravelRoute', () => {
-  let pathToFile: ['cache', 'test']
-  let fileNames: [
-    'input-routes-1.csv',
-    'input-routes-2.csv'
-  ]
+  let travelRouteRepository: ITravelRouteRepository
 
-  before(() => {
-    pathToFile = ['cache', 'test']
-    fileNames = ['input-routes-1.csv', 'input-routes-2.csv']
+  let pathToFile: string
+  let sourceOfPathToFile: string
+
+  before(async () => {
+    const write = promisify(fs.writeFile)
+
+    pathToFile = path.join(
+      'cache',
+      'test',
+      '0e2810b9-dd7e-4c7d-86a2-7d29b94a14b3-CreateTravelRouteIntegration.file.txt'
+    )
+
+    sourceOfPathToFile = path.join(
+      'cache',
+      'test',
+      'ebc1a29a-2a1c-402e-9b64-be6456e83147-CreateTravelRouteIntegration.source.txt'
+    )
+
+    travelRouteRepository = new TravelRouteRepository(fileAccess, sourceOfPathToFile)
+
+    await write(sourceOfPathToFile, pathToFile)
 
     chai.use(chaiAsPromised)
 
@@ -23,7 +41,6 @@ describe('INTEGRATION | CreateTravelRoute', () => {
 
   context('When I have to create a new route using a file repository', () => {
     it('Should persist one route on a file', async () => {
-      const travelRouteRepository = new TravelRouteRepository(pathToFile, fileNames[0])
       const createTravelRoute = new CreateTravelRoute(travelRouteRepository)
 
       return expect(createTravelRoute.execute({ routeName: 'GRU-SCL', routePrice: 500 }))
@@ -33,7 +50,6 @@ describe('INTEGRATION | CreateTravelRoute', () => {
     })
 
     it('Should persist two routes on a file', async () => {
-      const travelRouteRepository = new TravelRouteRepository(pathToFile, fileNames[1])
       const createTravelRoute = new CreateTravelRoute(travelRouteRepository)
 
       await createTravelRoute.execute({
@@ -51,7 +67,7 @@ describe('INTEGRATION | CreateTravelRoute', () => {
   after(async () => {
     const deleteFile = promisify(fs.unlink)
 
-    const promisesToDeleteFiles = pathToFile.map(path => deleteFile(path))
+    const promisesToDeleteFiles = [pathToFile, sourceOfPathToFile].map(path => deleteFile(path))
 
     await Promise.all(promisesToDeleteFiles)
   })
