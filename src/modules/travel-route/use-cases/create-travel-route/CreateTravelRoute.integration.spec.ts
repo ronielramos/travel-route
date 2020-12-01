b1ffec3a2c0a40a8d7cb166c527658d46eac2e0d
Roniel <roniel_ramos@hotmail.com>
2020-12-01 01:20:41 -0300

    test(CreateTravelRoute): deletar arquivo e recriar a cada teste de integração executado

--- src/modules/travel-route/use-cases/create-travel-route/CreateTravelRoute.integration.spec.ts
+++ src/modules/travel-route/use-cases/create-travel-route/CreateTravelRoute.integration.spec.ts
@@ -1,5 +1,4 @@
-import chai, { expect, should } from 'chai'
-import chaiAsPromised from 'chai-as-promised'
+import { should } from 'chai'
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
@@ -12,11 +11,27 @@ import CreateTravelRoute from './CreateTravelRoute'
 describe('INTEGRATION | CreateTravelRoute', () => {
   let travelRouteRepository: ITravelRouteRepository
 
-  let pathToFile: string
+  let readFile: (file: string) => Promise<Buffer>
+  let deleteFile: (file: string) => Promise<void>
+  let writeFile: (file: string, data: string) => Promise<void>
+
   let sourceOfPathToFile: string
+  let pathToFile: string
 
   before(async () => {
-    const write = promisify(fs.writeFile)
+    should()
+
+    readFile = promisify(fs.readFile)
+    writeFile = promisify(fs.writeFile)
+    deleteFile = promisify(fs.unlink)
+  })
+
+  beforeEach(async () => {
+    sourceOfPathToFile = path.join(
+      'cache',
+      'test',
+      'ebc1a29a-2a1c-402e-9b64-be6456e83147-CreateTravelRouteIntegration.source.txt'
+    )
 
     pathToFile = path.join(
       'cache',
@@ -24,49 +39,44 @@ describe('INTEGRATION | CreateTravelRoute', () => {
       '0e2810b9-dd7e-4c7d-86a2-7d29b94a14b3-CreateTravelRouteIntegration.file.txt'
     )
 
-    sourceOfPathToFile = path.join(
-      'cache',
-      'test',
-      'ebc1a29a-2a1c-402e-9b64-be6456e83147-CreateTravelRouteIntegration.source.txt'
-    )
+    await writeFile(sourceOfPathToFile, pathToFile)
 
-    travelRouteRepository = new TravelRouteRepository(fileAccess, sourceOfPathToFile)
-
-    await write(sourceOfPathToFile, pathToFile)
-
-    chai.use(chaiAsPromised)
-
-    should()
+    travelRouteRepository = new TravelRouteRepository(fileAccess)
+    await travelRouteRepository.initialize(sourceOfPathToFile)
   })
 
   context('When I have to create a new route using a file repository', () => {
     it('Should persist one route on a file', async () => {
       const createTravelRoute = new CreateTravelRoute(travelRouteRepository)
 
-      return expect(createTravelRoute.execute({ routeName: 'GRU-SCL', routePrice: 500 }))
-        .to
-        .be
-        .fulfilled
+      await createTravelRoute.execute({ routeName: 'GRU-GDO', routePrice: 200 })
+
+      const routesBuffer = await readFile(pathToFile)
+      const routes = routesBuffer
+        .toString()
+        .split('\n')
+        .filter(route => route !== '')
+
+      routes.should.have.lengthOf(1)
     })
 
     it('Should persist two routes on a file', async () => {
       const createTravelRoute = new CreateTravelRoute(travelRouteRepository)
 
-      await createTravelRoute.execute({
-        routeName: 'GRU-SCL',
-        routePrice: 1_000
-      })
+      await createTravelRoute.execute({ routeName: 'GRU-SCL', routePrice: 1_000 })
+      await createTravelRoute.execute({ routeName: 'SCL-GRU', routePrice: 500 })
 
-      return expect(createTravelRoute.execute({ routeName: 'SCL-GRU', routePrice: 500 }))
-        .to
-        .be
-        .fulfilled
+      const routesBuffer = await readFile(pathToFile)
+      const routes = routesBuffer
+        .toString()
+        .split('\n')
+        .filter(route => route !== '')
+
+      routes.should.have.lengthOf(2)
     })
   })
 
-  after(async () => {
-    const deleteFile = promisify(fs.unlink)
-
+  afterEach(async () => {
     const promisesToDeleteFiles = [pathToFile, sourceOfPathToFile].map(path => deleteFile(path))
 
     await Promise.all(promisesToDeleteFiles)
