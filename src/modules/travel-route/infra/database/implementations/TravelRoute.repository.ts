ad9e1541f9cf9388e7d6e7cf40f368e02420ad5a
Roniel <roniel_ramos@hotmail.com>
2020-11-30 23:31:28 -0300

    refactor: receber nome do arquivo que contém endereço do arquivo que servirá como repositório
    inicializar repositório usando endereço gravado no arquivo recebido

---
+++ src/modules/travel-route/infra/database/implementations/TravelRoute.repository.ts
@@ -0,0 +1,63 @@
+import { IFileAccess } from '../../../../../shared/providers/file-access/IFileAccess'
+import { CreatedTravelRouteDTO, TravelRouteToCreateDTO } from '../../../dtos/CreateTravelRoute.dto'
+import { ITravelRouteRepository } from '../ITravelRoute.repository'
+
+export default class TravelRouteRepository implements ITravelRouteRepository {
+  private fileAddress?: string
+
+  constructor (
+    private fileAccess: IFileAccess,
+    public readonly fileNameToInitilize: string
+  ) {
+    this.initialize(fileNameToInitilize)
+  }
+
+  async initialize (file: string) {
+    const fileToPersist = await this.fileAccess.read(file)
+
+    this.fileAddress = fileToPersist
+      .toString()
+      .split('\n')
+      .pop()
+  }
+
+  async create (data: TravelRouteToCreateDTO): Promise<CreatedTravelRouteDTO> {
+    if (!this.fileAddress) throw new Error('File to persist data was not provided!')
+
+    const [origin, destination] = data.routeName.split('-') as [string, string]
+    const routeToSaveOnFile = `${origin},${destination},${data.routePrice}`
+
+    await this.fileAccess.write(this.fileAddress, routeToSaveOnFile)
+
+    return {
+      origin,
+      destination,
+      price: data.routePrice
+    }
+  }
+
+  async getAll (): Promise<CreatedTravelRouteDTO[]> {
+    if (!this.fileAddress) throw new Error('File to persist data was not provided!')
+
+    const text = await this.fileAccess.read(this.fileAddress)
+
+    const travelRoutes = text
+      .toString()
+      .split('\n')
+      .map(line => {
+        const routeData = line.split(',')
+
+        const origin = routeData[0] ?? ''
+        const destination = routeData[1] ?? ''
+        const price = parseInt(routeData[2] ?? '') || 0
+
+        return {
+          origin,
+          destination,
+          price
+        }
+      })
+
+    return travelRoutes
+  }
+}
