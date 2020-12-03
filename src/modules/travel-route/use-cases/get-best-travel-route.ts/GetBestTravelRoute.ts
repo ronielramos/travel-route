97dd460891af72dec5077e3b42ae400b49635240
Roniel <roniel_ramos@hotmail.com>
2020-12-02 23:54:27 -0300

    refactor: buscar domínio através da factory

--- src/modules/travel-route/use-cases/get-best-travel-route.ts/GetBestTravelRoute.ts
+++ src/modules/travel-route/use-cases/get-best-travel-route.ts/GetBestTravelRoute.ts
@@ -1,17 +1,19 @@
-import BestTravelRoute from '../../domain/BestTravelRoute'
+import { IBestTravelRouteFactory } from '../../domain/factories/IBestTravelRouteFactory'
 import { TravelRouteFoundDTO, TravelRouteToFindDTO } from '../../dtos/GetTravelRoute.dto'
 import { ITravelRouteRepository } from '../../infra/database/ITravelRoute.repository'
 import { IUseCase } from '../IUseCase'
 
 export default class GetBestTravelRoute implements IUseCase<TravelRouteToFindDTO, TravelRouteFoundDTO> {
   constructor (
-    private bestTravelRoute: BestTravelRoute,
+    private bestTravelRouteFactory: IBestTravelRouteFactory,
     private travelRouteRepository: ITravelRouteRepository
   ) {}
 
-  async execute (data: TravelRouteToFindDTO): Promise<TravelRouteFoundDTO> {
+  async execute ({ origin, destination }: TravelRouteToFindDTO): Promise<TravelRouteFoundDTO> {
     const travelRoutes = await this.travelRouteRepository.getAll()
-    const bestRouteFound = this.bestTravelRoute.find(data, travelRoutes)
+    const bestRouteFound = this.bestTravelRouteFactory
+      .makeBestTravelRoute()
+      .find({ origin, destination }, travelRoutes)
 
     return bestRouteFound
   }
