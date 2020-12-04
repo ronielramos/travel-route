28d2c6686f7da6b4d6db933e0aae7dff18a707ac
Roniel <roniel_ramos@hotmail.com>
2020-12-03 21:13:25 -0300

    test: adicionar casos necessários para validação da integração

---
+++ src/modules/travel-route/use-cases/get-best-travel-route.ts/GetBestTravelRoute.integration.spec.ts
@@ -0,0 +1,73 @@
+import chai, { should } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import path from 'path'
+
+import { fileAccess } from '../../../../shared/providers/file-access'
+import { BestTravelRouteError } from '../../domain/errors/BestTravelRouteError'
+import { BestTravelRouteFactory } from '../../domain/factories/BestTravelRouteFactory'
+import { TravelRouteFoundDTO, TravelRouteToFindDTO } from '../../dtos/GetTravelRoute.dto'
+import TravelRouteRepository from '../../infra/database/repositories/implementations/TravelRoute.repository'
+import { ITravelRouteRepository } from '../../infra/database/repositories/ITravelRoute.repository'
+import { IUseCase } from '../IUseCase'
+import GetBestTravelRouteUseCase from './GetBestTravelRoute.use-case'
+
+describe('INTEGRATION | GetBestTravelRoute', () => {
+  let travelRouteRepository: ITravelRouteRepository
+  let getBestTravelRouteUseCase: IUseCase<TravelRouteToFindDTO, TravelRouteFoundDTO>
+  let sourceOfPathToFile: string
+  let pathToFile: string
+
+  before(() => {
+    should()
+    chai.use(chaiAsPromised)
+  })
+
+  beforeEach(async () => {
+    const bestTravelRouteFactory = new BestTravelRouteFactory()
+
+    const pathToSourceFileName = 'bd9314ae-1b54-4fc4-96bb-eaf1dfa5fb4a.txt'
+    sourceOfPathToFile = path.join('cache', 'test', pathToSourceFileName)
+
+    const sourceFileName = '57125b23-3e75-4d72-844a-b7155d0a0860.txt'
+    pathToFile = path.join('cache', 'test', sourceFileName)
+
+    await fileAccess.write(sourceOfPathToFile, pathToFile)
+
+    travelRouteRepository = new TravelRouteRepository(fileAccess)
+
+    await travelRouteRepository.initialize(sourceOfPathToFile)
+
+    await travelRouteRepository.create({
+      routeName: 'GRU-BRC',
+      routePrice: 10
+    })
+
+    getBestTravelRouteUseCase = new GetBestTravelRouteUseCase(
+      bestTravelRouteFactory,
+      travelRouteRepository
+    )
+  })
+
+  context('When I have to return the best price for a route', () => {
+    it('Should find the best path for route GRU-BRC', async () => {
+      const route = await getBestTravelRouteUseCase.execute({ origin: 'GRU', destination: 'BRC' })
+      route.should.be.equal('GRU - BRC > $10')
+    })
+
+    it('Should not find any route to an invalid route origin', () => {
+      return getBestTravelRouteUseCase.execute({
+        origin: '75659fb2-710d-497e-81e2-b63428076b76',
+        destination: 'BRC'
+      })
+        .should
+        .be
+        .rejectedWith(BestTravelRouteError)
+    })
+
+    afterEach(async () => {
+      const promisesToDeleteFiles = [pathToFile, sourceOfPathToFile].map(path => fileAccess.remove(path))
+
+      await Promise.all(promisesToDeleteFiles)
+    })
+  })
+})
